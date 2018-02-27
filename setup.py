20076d4c3ee69d04b53843cc9dae583546845fce
Thomas Neumann <neumann.thomas@gmail.com>
2018-02-27 11:16:05 +0100

    use system-wide pybind11 include instead of shipping an outdated version

--- setup.py
+++ setup.py
@@ -6,12 +6,28 @@ import setuptools
 
 
 
+class get_pybind_include(object):
+    """Helper class to determine the pybind11 include path
+    The purpose of this class is to postpone importing pybind11
+    until it is actually installed, so that the ``get_include()``
+    method can be invoked. """
+
+    def __init__(self, user=False):
+        self.user = user
+
+    def __str__(self):
+        import pybind11
+        return pybind11.get_include(self.user)
+
+
+
 ext_modules = [
     Extension(
         'cgtools.fastmath._fastmath_ext',
         ['src/fastmath.cpp'],
         include_dirs=[
-            'include',
+            get_pybind_include(),
+            get_pybind_include(user=True),
             numpy.get_include(),
         ],
         language='c++'
