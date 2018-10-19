3766d4a0ebe9a8bdc9c5d432c2e09e7c88918e92
Thomas Neumann <neumann.thomas@gmail.com>
2018-10-19 10:17:13 +0200

    added rotation-only procrustes

--- cgtools/procrustes.py
+++ cgtools/procrustes.py
@@ -14,20 +14,26 @@ def procrustes3d(frompts, topts, allow_reflection=False):
     t1 = topts.mean(0)
     frompts_local = frompts - t0
     topts_local = topts - t1
-    # find best rotation - procrustes problem
-    M = np.dot(topts_local.T, frompts_local)
+    R = procrustes(frompts_local, topts_local, allow_reflection=allow_reflection)
+    T0 = np.eye(4)
+    T0[:3,:3] = R
+    T0[:3, 3] = t1 - np.dot(R, t0)
+    return T0
+
+
+def procrustes(frompts, topts, allow_reflection=False):
+    """
+    Finds a orthogonal rotation R \in SO(N) 
+    that aligns array of N-dimensional frompts with topts.
+
+    Solves the orthogonal procrustes problem.
+    """
+    M = np.dot(topts.T, frompts)
     U, s, Vt = np.linalg.svd(M)
-    #if np.linalg.det(np.dot(U, Vt)) < 0:
-    #    Vt[-1] *= -1
-    #R = np.dot(U, Vt)
     if allow_reflection:
         R = U.dot(Vt)
     else:
         d = np.sign(np.linalg.det(np.dot(Vt.T, U.T)))
         E = np.diag([1, 1, d])
         R = np.dot(U, np.dot(E, Vt))
-    T0 = np.eye(4)
-    T0[:3,:3] = R
-    T0[:3, 3] = t1 - np.dot(R, t0)
-    return T0
-
+    return R
