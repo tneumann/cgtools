2c8d62736a0c2aafc021618ec4d00a12093020ca
Thomas Neumann <neumann.thomas@gmail.com>
2018-11-15 20:52:59 +0100

    add generic load_mesh and save_mesh

---
+++ cgtools/io/__init__.py
@@ -0,0 +1,27 @@
+from functools import partial
+from os import path
+
+from .obj import load_obj, save_obj
+from .off import load_off, save_off
+
+
+def load_mesh(filename):
+    loaders = {
+        'obj': load_obj,
+        'off': partial(load_off, no_colors=True),
+    }
+    ext = path.splitext(filename)[1].lower()[1:]
+    if ext not in loaders:
+        raise IOError("No loader for %s extension known, available file formats are: %s" % (ext, loaders.keys()))
+    return loaders[ext](filename)
+
+
+def save_mesh(filename, verts, tris, *args, **kw):
+    writers = {
+        'obj': save_obj,
+        'off': save_off,
+    }
+    ext = path.splitext(filename)[1].lower()[1:]
+    if ext not in writers:
+        raise IOError("No known writer for %s extension known, available file formats are: %s" % (ext, loaders.keys()))
+    return writers[ext](filename, verts, tris, *args, **kw)
