e4491e567c80ae8cdea096caf22b70c84c0f23d9
Thomas Neumann <neumann.thomas@gmail.com>
2018-11-29 10:40:42 +0100

    fix test case

--- tests/test_vector.py
+++ tests/test_vector.py
@@ -1,11 +1,11 @@
 import numpy as np
+import numpy.testing as npt
 from cgtools import vector as V
 
 
 def test_inv_3x4():
-    Xs = np.random.random((2, 3, 4))
+    Xs = np.random.random((1000, 3, 4))
     Xs_inv_np = np.array([np.linalg.inv(V.to_4x4(Xi)) for Xi in Xs])
     Xs_inv_ours = V.inv_3x4(Xs)
-    np.allclose(Xs_inv_np[:, 3, :], (0, 0, 0, 1))
-    np.allclose(Xs_inv_ours, Xs_inv_np[:, :3, :])
-
+    assert np.allclose(Xs_inv_np[:, 3, :], np.array([0, 0, 0, 1])[np.newaxis])
+    npt.assert_allclose(Xs_inv_ours, Xs_inv_np[:, :3, :])
