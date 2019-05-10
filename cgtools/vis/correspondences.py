32e9f3a6bf74fc246d341002436252aaa570e653
Thomas Neumann <neumann.thomas@gmail.com>
2019-05-10 11:30:45 +0200

    add MultiMeshMorpher

--- cgtools/vis/correspondences.py
+++ cgtools/vis/correspondences.py
@@ -1,9 +1,11 @@
 import numpy as np
-from traits.api import Range, HasTraits
+import traits.api as ta
 from mayavi import mlab
 from tvtk.api import tvtk
 from tvtk.common import configure_input_data, configure_input
-from traitsui.api import View, Item
+import traitsui.api as tu
+
+from .mesh import mesh_as_vtk_actor
 
 
 def visualize_point_correspondences(source_pts, target_pts, ij_corr=None, scalars=None, point_size=10):
@@ -24,18 +26,18 @@ def visualize_point_correspondences(source_pts, target_pts, ij_corr=None, scalar
         actor.mapper.scalar_range = scalars.min(), scalars.max()
     mlab.gcf().scene.add_actor(actor)
 
-    class Ctrl(HasTraits):
-        alpha = Range(0., 1.)
+    class Ctrl(ta.HasTraits):
+        alpha = ta.Range(0., 1.)
 
         def _alpha_changed(self):
             pd.points = p + self.alpha * (p2 - p)
             mlab.gcf().scene.render()
 
-    Ctrl().edit_traits()
+    Ctrl().configure_traits()
 
 
-class Morpher(HasTraits):
-    alpha = Range(0.0, 1.0)
+class Morpher(ta.HasTraits):
+    alpha = ta.Range(0.0, 1.0)
 
     def __init__(self, verts1, verts2, tris=None, lines=None, as_points=False, 
                  scalars=None, scalars2=None, vmin=None, vmax=None,
@@ -48,7 +50,7 @@ class Morpher(HasTraits):
                 rep = 'wireframe'
         else:
             rep = 'surface'
-        HasTraits.__init__(self)
+        super(Morpher, self).__init__()
         self._verts1, self._verts2 = verts1, verts2
         self._polydata = tvtk.PolyData(points=verts1)
         if rep == 'points':
@@ -105,9 +107,71 @@ class Morpher(HasTraits):
             self._polydata.point_data.scalars = blended
         mlab.gcf().scene.render()
 
-    traits_view = View(Item('alpha', show_label=False), title='cgtools Morpher')
+    traits_view = tu.View(tu.Item('alpha', show_label=False), title='cgtools Morpher')
 
 
 def visualize_mesh_morph(verts1, verts2, tris=None, **kwargs):
     Morpher(verts1, verts2, tris, **kwargs).configure_traits()
 
+
+morph_mesh = visualize_mesh_morph
+
+
+class MultiMeshMorpher(ta.HasTraits):
+    visible = ta.Enum(values='_names')
+    morph_target = ta.Enum(values='_names')
+    morph_alpha = ta.Range(0.0, 1.0, 0.0)
+    show_edges = ta.Bool(False)
+    _names = ta.List()
+
+    def __init__(self, list_verts, tris, names=None, fig=None, **kw):
+        super(MultiMeshMorpher, self).__init__(**kw)
+        self._list_verts = list_verts
+        self._tris = tris
+
+        if fig is None:
+            self._fig = mlab.figure(bgcolor=(1, 1, 1))
+        else:
+            self._fig = fig
+
+        if names is None:
+            names = map(str, range(len(list_verts)))
+        self._names = list(names)
+
+        self._verts_by_name = dict(zip(self._names, list_verts))
+        self._actor, self._pd = mesh_as_vtk_actor(list_verts[0], tris, return_polydata=True)
+        self._actor.mapper.immediate_mode_rendering = True
+        self._actor.property.set(
+            ambient=0.0,
+            specular=0.15, 
+            specular_power=128., 
+            diffuse=0.8,
+        )
+        self._fig.scene.add_actor(self._actor)
+
+        self.visible = self._names[0]
+
+    @ta.on_trait_change('visible, show_edges, morph_target, morph_alpha')
+    def _update(self):
+        self._actor.property.edge_visibility = self.show_edges
+        v1 = self._verts_by_name[self.visible]
+        if self.morph_alpha > 0:
+            v2 = self._verts_by_name[self.morph_target]
+            self._pd.points = v1 * (1 - self.morph_alpha) + v2 * self.morph_alpha
+        else:
+            self._pd.points = v1
+        self._fig.scene.render()
+
+    view = tu.View(
+        tu.Group(
+            tu.Item('visible'),
+            tu.Item('morph_target'),
+            tu.Item('morph_alpha'),
+            tu.Item('show_edges', name='Wireframe'),
+            label="Viewer"),
+        title="MultiMeshMorpher"
+    )
+
+
+def morph_multimesh(list_verts, tris, **kw):
+    MultiMeshMorpher(list_verts, tris, **kw).configure_traits()
