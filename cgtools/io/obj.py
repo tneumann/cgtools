b56c4edb9599bfb2763fbd23630987a91b2d921c
Thomas Neumann <neumann.thomas@gmail.com>
2019-05-24 15:37:14 +0200

    various fixes in obj loader/saver
     - correctly handle full face definitions in saveobj
     - load file contents just once (faster)
     - automatically detect quads vs tris
     - correct order of return values in loadobj

--- cgtools/io/obj.py
+++ cgtools/io/obj.py
@@ -4,23 +4,21 @@ import re
 
 
 # TODO: use \d  in triangle regex instead of [^\/\s]
-_triangle_regex = re.compile("^f\s+([^\/\s]+)/?\S*/?\S*\s+([^\/\s]+)/?\S*/?\S*\s+([^\/\s]+)", re.MULTILINE)
-_quad_regex = re.compile("^f\s+(\d+)/?\S*/?\S*\s+(\d+)/?\S*/?\S*\s+(\d+)/?\S*/?\S*\s+(\d+)", re.MULTILINE)
-_triangle_regex_all = re.compile("^f\s+([^\/\s]+)/?([^\/\s]*)/?([^\/\s]*)\s+([^\/\s]+)/?([^\/\s]*)/?([^\/\s]*)\s+([^\/\s]+)/?([^\/\s]*)/?([^\/\s]*)", re.MULTILINE)
-_quad_regex_all = re.compile("f\s+(\d+)/?(\d+)?/?(\d+)?\s+(\d+)/?(\d+)?/?(\d+)?\s+(\d+)/?(\d+)?/?(\d+)?\s+(\d+)/?(\d+)?/?(\d+)?", re.MULTILINE)
-quad_regex_all = re.compile("^f\s+(\d+)/?(\d*)/?(\d*)\s+(\d+)/?(\d*)/?(\d*)\s+(\d+)/?(\d*)/?(\d*)\s+(\d+)/?(\d*)/?(\d*)", re.MULTILINE)
+_triangle_regex = re.compile(r"^f\s+(\d+)\S*\s+(\d+)\S*\s+(\d+)", re.MULTILINE)
+_quad_regex     = re.compile(r"^f\s+(\d+)\S*\s+(\d+)\S*\s+(\d+)\S*\s+(\d+)", re.MULTILINE)
+_triangle_regex_all = re.compile(r"^f\s+([\d]+)/?([\d]*)/?([\d]*)\s+([\d]+)/?([\d]*)/?([\d]*)\s+([\d]+)/?([\d]*)/?([\d]*)", re.MULTILINE)
+_quad_regex_all = re.compile(r"^f\s+(\d+)/?(\d+)?/?(\d+)?\s+(\d+)/?(\d+)?/?(\d+)?\s+(\d+)/?(\d+)?/?(\d+)?\s+(\d+)/?(\d+)?/?(\d+)?", re.MULTILINE)
 _normal_regex = re.compile("^vn\s+(\S+)\s+(\S+)\s+(\S+)", re.MULTILINE)
 _texcoord_regex = re.compile("^vt\s+(\S+)\s+(\S+)", re.MULTILINE)
 _vertex_regex = re.compile("^v\s+(\S+)\s+(\S+)\s+(\S+)", re.MULTILINE)
 _vertex_regex_with_color = re.compile("^v\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)", re.MULTILINE)
 
 
-def _array_fromregex(f, regex, dtype):
+def _array_fromregex(buffer, regex, dtype):
     # could use np.fromregex but that has a bug in some versions of numpy
     # that is caused when called with a compiled regex object
     # see https://github.com/numpy/numpy/pull/10501
-    content = f.read()
-    seq = regex.findall(content)
+    seq = regex.findall(buffer)
     return np.array(seq, dtype=dtype)
 
 
@@ -36,34 +34,48 @@ def load_texture_filename_in_obj(filename):
 
 
 def load_obj(filename, load_normals=False, load_texcoords=False, load_texture=False, 
-             load_full_face_definitions=False, is_quadmesh=False, load_colors=False):
+             load_full_face_definitions=False, is_quadmesh='auto', load_colors=False):
     """ load a wavefront obj file
         loads vertices into a (x,y,z) struct array and vertex indices
         into a n x 3 index array 
         only loads obj files vertex positions and also
         only works with triangle or (when is_quadmesh=True) with quad meshes """
+    contents = open(filename).read()
     if load_colors:
-        data = _array_fromregex(open(filename), _vertex_regex_with_color, np.float)
+        data = _array_fromregex(contents, _vertex_regex_with_color, np.float)
         vertices = data[:,:3]
         colors = data[:,3:]
     else:
-        vertices = _array_fromregex(open(filename), _vertex_regex, np.float)
+        vertices = _array_fromregex(contents, _vertex_regex, np.float)
 
     if load_normals:
-        normals = _array_fromregex(open(filename), _normal_regex, np.float)
+        normals = _array_fromregex(contents, _normal_regex, np.float)
     if load_texcoords:
-        texcoords = _array_fromregex(open(filename), _texcoord_regex, np.float)
-    if is_quadmesh:
-        reg = _quad_regex_all if load_full_face_definitions else _quad_regex
+        texcoords = _array_fromregex(contents, _texcoord_regex, np.float)
+
+    reg_quads = _quad_regex_all if load_full_face_definitions else _quad_regex
+    reg_tris = _triangle_regex_all if load_full_face_definitions else _triangle_regex
+    if is_quadmesh == 'auto':
+        quads = _array_fromregex(contents, reg_quads, np.int) - 1 # 1-based indexing in obj file format!
+        if len(quads) > 0:
+            faces = quads
+        else:
+            faces = _array_fromregex(contents, reg_tris, np.int) - 1 # 1-based indexing in obj file format!
+    elif is_quadmesh:
+        quads = _array_fromregex(contents, reg_quads, np.int) - 1 # 1-based indexing in obj file format!
+        faces = quads
     else:
-        reg = _triangle_regex_all if load_full_face_definitions else _triangle_regex
-    faces = _array_fromregex(open(filename), reg, np.int) - 1 # 1-based indexing in obj file format!
+        tris = _array_fromregex(contents, reg_tris, np.int) - 1 # 1-based indexing in obj file format!
+        faces = tris
 
     r = [vertices]
     if load_normals:
         r.append(normals)
     if load_texcoords:
         r.append(texcoords)
+    if load_colors:
+        r.append(colors)
+
     r.append(faces)
 
     if load_texture:
@@ -75,9 +87,8 @@ def load_obj(filename, load_normals=False, load_texcoords=False, load_texture=Fa
         texture = imread(path.join(path.dirname(filename), tex_file))
         r.append(texture)
 
-    if load_colors:
-        r.append(colors)
     return r
+
 
 def save_obj(filename, vertices, faces, normals=None, texcoords=None, texture_file=None):
     with open(filename, 'w') as f:
@@ -98,28 +109,23 @@ def save_obj(filename, vertices, faces, normals=None, texcoords=None, texture_fi
                     "map_Kd %s\n" % texture_file
                 )
         np.savetxt(f, vertices, fmt="v %f %f %f")
-        n = 1
-        trifmt = "%d"
-        if not texcoords is None:
+        if texcoords is not None:
             np.savetxt(f, texcoords, 
                        fmt="vt %f %f")
-            n += 1
-            trifmt += "/%d"
 
-        if not normals is None:
+        if normals is not None:
             np.savetxt(f, normals, 
                        fmt="vn %f %f %f")
-            n += 1
-            if texcoords is None:
-                trifmt += "//%d"
-            else:
-                trifmt += "/%d"
 
         if texture_file is not None:
             f.write("usemtl material_0\n\n")
 
         if faces is not None and len(faces) > 0:
-            tris_duplicated = [faces + 1] * n
-            np.savetxt(f, np.dstack(tris_duplicated).reshape((-1, n*faces.shape[-1])), 
-                       fmt="f " + ' '.join([trifmt] * faces.shape[-1]))
+            if faces.shape[1] in [3, 4]:
+                face_fmt = 'f ' + ' '.join(['%d'] * faces.shape[1])
+            elif faces.shape[1] in [3*3, 3*4]:
+                face_fmt = 'f ' + ' '.join(['%d/%d/%d' * faces.shape[1]])
+            else:
+                raise ValueError("invalid format for faces, allowed: (N, 3), (N, 4), (N, 9), (N,12)")
+            np.savetxt(f, faces + 1, fmt=face_fmt)
 
