3b3a609e370c892f39d9b36177eb0dfed14490a5
Thomas Neumann <neumann.thomas@gmail.com>
2019-07-04 15:15:11 +0200

    use ipdb in debug if available

--- cgtools/debug.py
+++ cgtools/debug.py
@@ -1,6 +1,8 @@
-from IPython.core import debugger
-
+try:
+    import ipdb
+    debug = ipdb.set_trace
+except ImportError:
+    from IPython.core import debugger
+    debug = debugger.Pdb().set_trace
 
 __all__ = ['debug']
-
-debug = debugger.Pdb().set_trace
